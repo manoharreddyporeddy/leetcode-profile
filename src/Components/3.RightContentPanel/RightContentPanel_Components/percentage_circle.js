9a453a2c85a7502361feceee0a6d1d32fb6ad989
Akshay Varma <65845790+akshay722@users.noreply.github.com>
2021-08-11 09:25:12 +0530

    Dynamic Data

--- src/Components/3.RightContentPanel/RightContentPanel_Components/percentage_circle.js
+++ src/Components/3.RightContentPanel/RightContentPanel_Components/percentage_circle.js
@@ -2,35 +2,18 @@ import React from "react";
 import "./circle.css";
 
 const Percentage_circle = () => (
-  <div size="108" class="main">
-    <div radius="52.3125" class="main2"></div>
-    <svg
-      from="0"
-      height="108"
-      width="108"
-      size="108"
-      viewBox="0 0 108 108"
-      class="svg1"
-    >
-      <circle
-        cx="54"
-        cy="54"
-        r="50.625"
-        size="108"
-        class="circle1"
-        style={{ strokeWidth: "3.375px" }}
-      ></circle>
+  <div size="108" className="cicleContainer">
+    <div radius="52.3125" className="backgroundCircle"></div>
+    <svg viewBox="0 0 108 108" className="svgCircle">
       <circle
         from="0"
         to="70.73979335821748"
         cx="54"
         cy="54"
         r="50.625"
-        size="108"
         transform="rotate(0 54 54)"
         data-difficulty="Easy"
-        class="circle2"
-        // style={{ strokeWidth: "3.375px" }}
+        className="easyCircle"
       ></circle>
       <circle
         from="0"
@@ -38,10 +21,9 @@ const Percentage_circle = () => (
         cx="54"
         cy="54"
         r="50.625"
-        size="108"
         transform="rotate(80.06106870229007 54 54)"
         data-difficulty="Medium"
-        class="circle3"
+        className="mediumCircle"
       ></circle>
       <circle
         from="0"
@@ -49,15 +31,14 @@ const Percentage_circle = () => (
         cx="54"
         cy="54"
         r="50.625"
-        size="108"
         transform="rotate(150.59541984732823 54 54)"
         data-difficulty="Hard"
-        class="circle4"
+        className="hardCircle"
       ></circle>
     </svg>
-    <div class="text">
+    <div className="circleTextContainer">
       <div>
-        <div size="108" class="percent">
+        <div size="108" className="circelPercentage">
           <span>86</span>
           <span size="108" style={{ fontSize: "13.5px" }}>
             .7%
